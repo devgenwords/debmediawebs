7106b579a807b7c6316c336c1ad3f30a0366223d
Ezequiel de Valais <nicounyi@gmail.com>
2022-05-17 01:11:24 -0300

    mapss

--- components/Map.js
+++ components/Map.js
@@ -10,9 +10,23 @@ export default function Map() {
             <Container fluid className="map-section">
                 <Container>
                     <Row>
-                        <Col className="map-section_maintitle text-center">
-                           <img src={map.src} className="img-fluid map-section-maxMap" alt="Mapa"/>
+                        <Col className="map-section_mapcontent text-center">
+                            <img src={map.src} className="img-fluid map-section-maxMap" alt="Mapa" />
+                            <div className="numbers number1 text-center">
+                                <span className="quntity">+ 300</span>
+                                <span className="type">clientes</span>
+                            </div>
+                            <div className="numbers number2 text-center">
+                                300
+                            </div>
+                            <div className="numbers number3 text-center">
+                                300 
+                            </div>
+                            <div className="numbers number4 text-center">
+                                300
+                            </div>
                         </Col>
+
                     </Row>
                 </Container>
             </Container>
