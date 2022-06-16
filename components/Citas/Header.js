c3313b3fd56e4970707b1dd0df360b5ca09d1d16
naunyiciodeb <nicounyi@gmail.com>
2022-06-15 23:37:32 -0300

    citas y debq

--- components/Citas/Header.js
+++ components/Citas/Header.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Row, Container, Col } from "react-bootstrap";
 import Image from 'next/image'
-import mypic from '../../asset/imgs/debq/main.png'
+import mypic from '../../asset/imgs/citas/main.png'
 import shape1 from '../../asset/imgs/home/headershape1.svg'
 import shape2 from '../../asset/imgs/home/headershape2.svg'
 import shape3 from '../../asset/imgs/home/headershape3.svg'
@@ -15,31 +15,31 @@ export default function Header() {
     return (
         <>
             <header>
-                <Container fluid className="debq-home">
-                    <div className={'image-container-mobile'}>
+                <Container fluid className="citas-home">
+                    <div className={'image-container-citas'}>
                         <Image src={mypic.src} layout="fill" className={'image'} />
                     </div>
                     <div className="d-none d-sm-block">
-                        <img src={shape3.src} className="debq-home_shape3" />
+                        <img src={shape3.src} className="citas-home_shape3" />
                     </div>
                     <div className="d-none d-sm-block">
-                        <img src={mainNoti.src} className="debq-home_mainNoti" />
+                        <img src={mainNoti.src} className="citas-home_mainNoti" />
                     </div>
                     <div className="d-none d-sm-block">
-                        <img src={shape1.src} className="debq-home_shape1" />
-                        <img src={shape2.src} className="debq-home_shape2" />
+                        <img src={shape1.src} className="citas-home_shape1" />
+                        <img src={shape2.src} className="citas-home_shape2" />
                     </div>
                     <Container>
                         <Row>
                             <Col>
-                                <h1 className="debq-home_title">
-                                Mejora la eficiencia<br  className="d-none d-sm-block" />  y experiencia en tus<br  className="d-none d-sm-block" /> 
-sucursales
+                                <h1 className="citas-home_title">
+                                    Mejora la eficiencia<br className="d-none d-sm-block" />  y experiencia en tus<br className="d-none d-sm-block" />
+                                    sucursales
                                 </h1>
-                                <p className="debq-home_parraf">
+                                <p className="citas-home_parraf">
                                     Sistema de <b>gestión de turnos</b>
                                 </p>
-                                <button className="debq-home_demobutton">
+                                <button className="citas-home_demobutton">
                                     Solicitá una demo
                                 </button>
                             </Col>
@@ -47,10 +47,10 @@ sucursales
                     </Container>
                 </Container>
             </header>
-            <Container className="d-flex justify-content-center debq-home_text">
+            <Container className="d-flex justify-content-center citas-home_text">
                 <Row>
                     <Col>
-                        <h3><b>Organizaciones líderes</b> ya gestionan la <br className="d-none d-sm-block"/>
+                        <h3><b>Organizaciones líderes</b> ya gestionan la <br className="d-none d-sm-block" />
                             experiencia de sus clientes con <b> Debmedia</b></h3>
                     </Col>
                 </Row>
