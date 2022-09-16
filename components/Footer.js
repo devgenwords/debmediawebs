6481f5e6a3092da4d230b1b3a096ee07acfb72f8
Genwords <112586671+devgenwords@users.noreply.github.com>
2022-09-16 06:31:23 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -12,6 +12,12 @@ export default function Footer() {
     return (
         <>
             <footer>
+        
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T4XJ42L"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+        
                 <Container fluid className="footer-section">
                     <Container>
                         <Row>
