97af8e3ab873cd270f9c1b7d67e9d69cbd70d35b
Genwords <112586671+devgenwords@users.noreply.github.com>
2022-09-16 06:28:59 -0400

    Update Header.js

--- components/Debq/Header.js
+++ components/Debq/Header.js
@@ -15,6 +15,15 @@ export default function Header() {
     return (
         <>
             <header>
+        
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T4XJ42L');</script>
+<!-- End Google Tag Manager -->
+        
                 <Container fluid className="debq-home">
                     <div className={'image-container-debq'}>
                     <Image src={mypic.src} layout="fill" className={'image d-none d-sm-block'} />
